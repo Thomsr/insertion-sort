26162d816b42114640ddb29d9848e9176bd5ac1a
Thomas Rooskens <s3339661@vuw.leidenuniv.nl>
2023-02-09 16:33:54 +0100

    Sorting algorithm fixed

--- insertion-sort/src/scenes/introInsertionSort.tsx
+++ insertion-sort/src/scenes/introInsertionSort.tsx
@@ -2,6 +2,7 @@ import {makeScene2D} from '@motion-canvas/2d/lib/scenes';
 import {all, waitFor} from '@motion-canvas/core/lib/flow';
 import { Color } from '@motion-canvas/core/lib/types';
 import { createRef } from '@motion-canvas/core/lib/utils';
+import { Colors } from '../styles/styles';
 import { Array } from '../components/ArrayComponent/Array'
 
 export default makeScene2D(function* (view) {
@@ -22,17 +23,16 @@ export default makeScene2D(function* (view) {
 function * InsertionSort(Array: Array){
   for(let i = 1; i < Array.values().length; i++){
     let j = i;
-    while(j > 0 && Array.getValue(j-1) > Array.getValue(j)){
+    while(j >= 0 && Array.values()[j-1] > Array.values()[j]){
       yield* all(
-        Array.HighLight(j-1, 1, new Color('white')),
-        Array.HighLight(j, 1, new Color('white')),
+        Array.HighLight(j-1, 1, new Color(Colors.blue)),
+        Array.HighLight(j, 1, new Color(Colors.blue)),
       )
-      console.log("Here");
       yield * Array.Swap(j, j-1, true);
       yield* all(
-        Array.deHighLight(j-1, 1, new Color('#242424')),
-        Array.deHighLight(j, 1, new Color('#242424')),
-      )
+        Array.deHighLight(j-1, 1),
+        Array.deHighLight(j, 1),
+      );
       j -= 1;
     }
   }
