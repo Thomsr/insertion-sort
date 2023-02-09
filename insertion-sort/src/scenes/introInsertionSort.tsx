c2565cbd41d6e009692b5d6f0b8881cf2d9d34ff
Thomas Rooskens <s3339661@vuw.leidenuniv.nl>
2023-02-09 15:53:36 +0100

    Working on intro

--- insertion-sort/src/scenes/introInsertionSort.tsx
+++ insertion-sort/src/scenes/introInsertionSort.tsx
@@ -1,16 +1,39 @@
 import {makeScene2D} from '@motion-canvas/2d/lib/scenes';
-import {waitFor} from '@motion-canvas/core/lib/flow';
-import { Array } from '../components/Array'
+import {all, waitFor} from '@motion-canvas/core/lib/flow';
+import { Color } from '@motion-canvas/core/lib/types';
+import { createRef } from '@motion-canvas/core/lib/utils';
+import { Array } from '../components/ArrayComponent/Array'
 
 export default makeScene2D(function* (view) {
   // Create your animations here
+  const ArrayReference = createRef<Array>();
 
   view.add(
     <Array
-      values={[1,2]}
-    >
-
-    </Array>
+      ref={ArrayReference}
+      values={[1, 3, 6, 2]}
+    />
   )
+
+  yield* InsertionSort(ArrayReference());
   yield* waitFor(5);
 });
+
+function * InsertionSort(Array: Array){
+  for(let i = 1; i < Array.values().length; i++){
+    let j = i;
+    while(j > 0 && Array.getValue(j-1) > Array.getValue(j)){
+      yield* all(
+        Array.HighLight(j-1, 1, new Color('white')),
+        Array.HighLight(j, 1, new Color('white')),
+      )
+      console.log("Here");
+      yield * Array.Swap(j, j-1, true);
+      yield* all(
+        Array.deHighLight(j-1, 1, new Color('#242424')),
+        Array.deHighLight(j, 1, new Color('#242424')),
+      )
+      j -= 1;
+    }
+  }
+}
