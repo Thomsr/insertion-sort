fe88609b6fbadcb44fc1194f4f6679be0fb0d42e
Thomas Rooskens <s3339661@vuw.leidenuniv.nl>
2023-02-09 20:30:06 +0100

    Added Outline

--- insertion-sort/src/scenes/introInsertionSort.tsx
+++ insertion-sort/src/scenes/introInsertionSort.tsx
@@ -5,72 +5,77 @@ import { Rect } from '@motion-canvas/2d/lib/components/'
 import { createRef, useLogger } from '@motion-canvas/core/lib/utils';
 import { Colors } from '../styles/styles';
 import { Array } from '../components/ArrayComponent/Array'
-import { createSignal } from '@motion-canvas/core/lib/signals';
+import { createSignal, Signal, SignalValue, SimpleSignal } from '@motion-canvas/core/lib/signals';
 
 export default makeScene2D(function* (view) {
-  // Create your animations here
   const ArrayReference = createRef<Array>();
   const outlineReference = createRef<Rect>();
 
-  const ArrayValues = [1, 3, 6, 2, 4, 6];
-  const BoxWidth = createSignal(2);
+  const ArrayValues = [1, 3, 6, 2, 4];
+  const BoxWidth = createSignal(1);
 
   view.add(
     <>
-      <Rect
-        ref={outlineReference}
-        x={() => -(ArrayValues.length/2) * (128 + 28) + BoxWidth() * ((128 + 28) / 2)}
-        height={168}
-        width={() => BoxWidth() * (128 + 28) + 20}
-        stroke={Colors.green}
-        lineWidth={8}
-        radius={new Spacing(4)}
-      >
-      </Rect>
       <Array
         ref={ArrayReference}
         values={ArrayValues}
+        boxGap={36}
       />
+      <Rect
+        ref={outlineReference}
+        x={() => -(ArrayValues.length/2) * (128 + 36) + BoxWidth() * ((128 + 36) / 2)}
+        height={168}
+        width={() => BoxWidth() * (128 + 36)}
+        stroke={Colors.green}
+        lineWidth={6}
+        radius={new Spacing(4)}
+      >
+      </Rect>
     </>
   )
-  yield* updateOutline(ArrayReference(), outlineReference())
 
-  yield* InsertionSort(ArrayReference());
-  yield* waitFor(5);
+  yield* InsertionSort(ArrayReference(), outlineReference(), BoxWidth);
+  yield* waitFor(1);
 });
 
 function * updateOutline(Array: Array, Outline: Rect){
+  const offSet = 24;
   for(let i = 0; i < Array.values().length; i++){
-    if(Array.boxArray[i].absolutePosition().x < Outline.absolutePosition().x){
-      useLogger().debug(i.toString() + ' ' + Array.boxArray[i].absolutePosition() + ' ' + Outline.absolutePosition().x)
-      Array.boxArray[i].position.x(Array.boxArray[i].position.x() - 100);
+    if(Array.children()[i].absolutePosition().x < Outline.absolutePosition().x + Outline.size().x /2){
+      Array.boxArray[i].position.x(Array.boxArray[i].position.x() - offSet);
+      Outline.position.x(Outline.position.x() -  offSet/i)
     }
   }
 }
 
-function * InsertionSort(Array: Array){
+function * InsertionSort(Array: Array, Outline: Rect, BoxWidth: SimpleSignal<number>){
   for(let i = 1; i < Array.values().length; i++){
+    yield BoxWidth(i, 1);
+    yield* waitFor(1);
+    // yield* updateOutline(Array, Outline);
     let j = i;
+    useLogger().debug(BoxWidth().toString());
     while(j >= 0){
-      // yield* all(
-      //   Array.HighLight(j-1, .5, new Color(Colors.blue)),
-      //   Array.HighLight(j, .5, new Color(Colors.blue)),
-      // );
-
+      yield* all(
+        Array.HighLight(j-1, 1, new Color(Colors.blue)),
+        Array.HighLight(j, 1, new Color(Colors.blue)),
+      );
+      waitFor(1);
       if(Array.values()[j-1] > Array.values()[j]){
-        yield * Array.Swap(j, j-1, true, 0.5);
-      } else {
-        // yield* all(
-        //   Array.deHighLight(j-1, 1),
-        //   Array.deHighLight(j, 1),
-        // );
+        yield * Array.Swap(j, j-1, true, 1);
+      } else{
+        yield* all(
+          Array.deHighLight(j-1, 1),
+          Array.deHighLight(j, 1),
+        );
         break;
       }
-      // yield* all(
-      //   Array.deHighLight(j-1, .2),
-      //   Array.deHighLight(j, .2),
-      // );
+      yield* all(
+        Array.deHighLight(j-1, 1),
+        Array.deHighLight(j, 1),
+      );
       j -= 1;
     } 
   }
+  yield BoxWidth(Array.values().length, 1);
 }
